d6c742cfba9ac56d03b85fce1f780e8f0978eec0
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-08-13 18:53:19 -0400

    changing str to int

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -3,10 +3,11 @@ import random
 print("The Number Guessing Game")
 print()
 
-#Asking user how big of a number they want to guess
+# Asking & confirming with user how big of a number they want to guess
 while True:
     print()
-    digits = input("Enter the size of the number you want to guess - must be between 3 and 8 digits long (bigger numbers are harder to guess):   ")
+    digits = input(
+        "Enter the size of the number you want to guess - must be between 3 and 8 digits long (bigger numbers are harder to guess):   ")
 
     if (not(digits.isnumeric())):
         print("You didn't enter a number. Try again.")
@@ -36,13 +37,13 @@ while True:
         else:
             print("You did not choose a number between 3 and 8. Try again.")
 
-#rules of the game
+# rules of the game
 print()
 print("Now that the computer has its number, here are the rules.")
 print("""
 1. The first number cannot be a zero.
 2. No digit can be repeated. 
-For example, the computer would never pick 1221 or 1231 as the same number shows up more than once.
+For example, the computer would never pick 1231 as the same number - 1 - shows up more than once.
 3. You must enter the correct size number in a guess. 
 Don't guess 1234 if you wanted the computer to have a 3 digit number.
 4. Once you've guessed, the computer will tell you how many numbers are correct and how many numbers are in the correct spot. 
@@ -50,14 +51,13 @@ For example, if the number is 123 and you pick 135, the computer will say: You h
 5. Game ends once you've picked the correct number. Good luck!
 """)
 
-#computer picks the number - no zero as first digit
+# computer picks the number - no zero as first digit & no repeats
 computer_guess = [0]
 while computer_guess[0] == 0:
     computer_guess = random.sample(range(0, 10), digits)
 print(computer_guess)
 
-'''
-#time to play!
+# user guesses and computer checks against answer
 while True:
     print()
     guess = input(
@@ -69,8 +69,17 @@ while True:
         print("You did not enter a correctly-sized guess. Try again.")
         continue
     else:
-        guess = guess.split()
+        guess = list(guess) #puts string into list of strings
+        guess = [int(i) for i in guess] #converts str to int
         if (guess == computer_guess):
-            print(f"Congrats, you've won! The correct number is {computer_guess}! Great work!!")
+            computer_guess = [str(i) for i in computer_guess] #converts int to str
+            computer_guess = str("".join(computer_guess)) #joins all ints 
+            print(
+                f"Congrats, you've won! The correct number is {computer_guess}! Great work!!") 
             break
-'''        
+        else:
+            #find number of correct digits
+            correct_digits = list(i for i in computer_guess if i in guess)
+            num_correct_digits = len(correct_digits)
+            print(f"You have correctly guessed {num_correct_digits} digits.")
+            break
