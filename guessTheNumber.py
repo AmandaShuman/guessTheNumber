9fd565f680a1640e9880a9b61a5177d453f1316b
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-08-13 10:36:53 -0400

    checking user input

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -1,3 +1,15 @@
-'''
+print("The Number Guessing Game")
+while True:
+    print()
+    digits = input("Enter the size of the number you want to guess - must be between 3 and 8 digits long:   ")
 
-'''
+    if (not(digits.isnumeric())):
+        print("You didn't enter a number. Try again.")
+    else:
+        digits = int(digits)
+        if (3 <= digits <= 8):
+            print(f"You have chosen to guess a random number that contains {digits} digits long.")
+            break
+        else:
+            print("You did not choose a number between 3 and 8. Try again.")
+            continue
